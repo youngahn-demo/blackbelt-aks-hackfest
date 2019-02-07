e6108b369d699b12ea3869138ab4b302cff66d2d
youngahn-demo <47435505+youngahn-demo@users.noreply.github.com>
2019-02-07 15:08:02 -0800

    Create brigade.js

---
+++ brigade.js
@@ -0,0 +1,7 @@
+function kubeJobRunner (config, k) {
+    k.storage.enabled = false
+    k.image = "lachlanevenson/k8s-kubectl:v1.8.2"
+    k.tasks = [
+        `kubectl set image deployment/heroes-web-deploy heroes-web-cntnr=ytahnrepo.azurecr.io.azurecr.io/azureworkshop/rating-web:${config.get("imageTag")}`
+    ]
+}
