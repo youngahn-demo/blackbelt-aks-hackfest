cece273b63cc2cba3c8912ac02fc09c9675299e6
youngahn-demo <47435505+youngahn-demo@users.noreply.github.com>
2019-02-07 15:31:21 -0800

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        장난하냐 Team
+        I dunno Team
       </div>
       <div class="col-lg-6">
       </div>
