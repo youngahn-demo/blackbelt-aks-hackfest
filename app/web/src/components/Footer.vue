92253e8b100baffd56b90921e07ce6b81333d8a9
youngahn-demo <47435505+youngahn-demo@users.noreply.github.com>
2019-02-07 23:18:10 -0800

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        TEST TEST TEST TEST TEST TEST Team
+        TEST TEST TEST TEST TEST TEST TEST Team
       </div>
       <div class="col-lg-6">
       </div>
